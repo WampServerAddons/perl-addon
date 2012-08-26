1a2a199c1476377cb68e894cb3d8c373465b58ae
Lehman Black <dosnlinux@gmail.com>
2012-08-25 19:59:10 -0500

    first draft of installer

---
+++ README.txt
@@ -0,0 +1,31 @@
+Perl Add-on for WampServer Version 2.2a
+
+Apache version: 2.2.21
+
+About:
+ This is an addon for WampServer 2.2a that enables use of the Perl programming language
+
+Where to download Perl:
+ Strawberry Perl home page - http://strawberryperl.com/
+ Strawberry Perl binaries - http://code.google.com/p/strawberry-perl/downloads/list
+
+Manual install instructions:
+ (assumes wamp is already installed and working)
+
+ 1. download Perl from the site listed above.
+ 2. Run the installer for Perl. Be sure to tell it to install the files in
+    c:\wamp\bin\perl\perl%PERL_VERSION
+ 3. add c:\wamp\bin\perl\perl%PERL_VERSION%\bin to your %PATH%
+    (a logoff or reboot maybe required)
+ 4. restart Wamp
+
+Using the Installer:
+ usage: installer.bat
+
+
+TODO:
+ * also install mod_perl
+ * integration with wampserver manager menu on systray
+
+FIXME:
+ * spelling/capitalization check on all files
---
+++ installer.bat
@@ -0,0 +1,52 @@
+@echo OFF
+REM TODO use the call command to set some variables common to both the installer/uninstaller
+set PERL_VERSION=5.12.3.0
+set APACHE_VERSION=2.2.21
+set WAMP_VERSION=2.2a
+
+set ADDON=Perl
+
+set BIN=installer\bin
+set TMP=installer\temp
+
+set WAMP=c:\wamp
+set WAMP_PERL=%WAMP%\bin\perl
+
+set PERL_FILE=strawberry-perl-%PERL_VERSION%.msi
+set PERL_DIR=perl%PERL_VERSION%
+set PERL_BIN=%WAMP_PERL%\%PERL_DIR%
+
+set PERL_DOWNLOAD=http://strawberry-perl.googlecode.com/files/%PERL_FILE%
+
+set PATH=%PATH%;%BIN%
+
+echo Welcome to the %ADDON% Addon installer for WampServer %WAMP_VERSION%
+
+REM set up the temp directory
+IF NOT EXIST %TMP% GOTO MKTMP
+echo 	Temp directory found from previous install: DELETING
+rd /S /Q %TMP%
+
+:MKTMP
+echo 	Setting up the temp directory...
+mkdir %TMP%
+
+REM download Perl files to temp directory
+echo 	Downloading %ADDON% binaries to temp directory...
+wget.exe -nd -q -P %TMP% %PERL_DOWNLOAD%
+
+REM install the binary files in the WampServer install directory
+echo 	Installing %ADDON% to the WampServer install directory...
+msiexec /i %TMP%\%PERL_FILE% /passive TARGETDIR=%PERL_BIN%
+
+REM add the Perl bin directory to the PATH so apache can find them
+echo 	Setting enviorment variables...
+setenv -a PATH %PERL_BIN%\bin
+
+REM clean up temp files
+echo 	Cleaning up temp files...
+rd /S /Q %TMP%
+
+echo %ADDON% is installed successfully. Please restart WampServer.
+
+pause
